fd6f4b6eec50a380df25c74c6c717de4196db40e
Magzhan <magzhankarataev02@gmail.com>
2025-08-16 15:40:09 +0500

    update: new folders

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
 import "@/styles/tailwind.css"
 import { ReactElement } from "react"
 import { I18nProviderClient } from "@/locale/client"
-import { TonProvider, Web3Provider } from "@/shared/components/web3"
+import { TonProvider, Web3Provider } from "@/shared/components/elements/web3"
 
 export default async function RootLayout({ params, children }: { params: Promise<{ locale: string }>, children: ReactElement }) {
   const { locale } = await params
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,15 @@
-import { TonConnect, WalletConnect } from "@/shared/components/web3"
+import { TonConnect, WalletConnect } from "@/shared/components/elements/web3"
 
 export default function Main() {
   return (
-    <div className="container mx-auto py-8">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-4xl">
+    <div className="mx-auto py-8 container">
+      <div className="gap-8 grid grid-cols-1 md:grid-cols-2 max-w-4xl">
         <div>
-          <h2 className="text-xl font-semibold mb-4">Connect Ethereum Wallet</h2>
+          <h2 className="mb-4 font-semibold text-xl">Connect Ethereum Wallet</h2>
           <WalletConnect />
         </div>
         <div>
-          <h2 className="text-xl font-semibold mb-4">Connect TON Wallet</h2>
+          <h2 className="mb-4 font-semibold text-xl">Connect TON Wallet</h2>
           <TonConnect />
         </div>
       </div>

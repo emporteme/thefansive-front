84a8942ddb36f08428fe800c48f23765c1525bfd
Tamerlan Telgarin <t.telgarin@qomek.kz>
2025-08-23 22:05:13 +0500

    feature/KAN-23: added containers pages folder

--- src/app/[locale]/(mvp)/user/profile/page.tsx
+++ src/app/[locale]/(mvp)/user/profile/page.tsx
@@ -1,7 +1,8 @@
+import ProfilePage from "@/containers/pages/profile-page/profile-page"
 import { FC } from "react"
 
 const Page: FC = () => {
-  return <div>Profile (mvp)</div>
+  return <ProfilePage />
 }
 
 export default Page
---
+++ src/containers/pages/profile-page/profile-page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { FC } from "react"
+
+const ProfilePage: FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+       <div>Profile (mvp)</div>
+    </div>
+  )
+}
+
+export default ProfilePage

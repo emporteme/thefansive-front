7d66cd0fee2d53ec86e5bf552296a1af04c59576
Tamerlan Telgarin <t.telgarin@qomek.kz>
2025-08-26 02:28:57 +0500

    feature/KAN-23: user layout banner empty and fix styles

--- src/containers/layout/user/user.tsx
+++ src/containers/layout/user/user.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import React from "react"
 import { SidebarSection } from "./components/sections"
 
@@ -8,7 +9,15 @@ interface UserLayoutProps {
 const UserLayout: React.FC<UserLayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col">
-      <div className="mt-7 mb-16 flex h-[160px] w-full items-center justify-center bg-gray-200">Image</div>
+      <div className="mt-7.5 mb-18 h-[160px] w-full border-t border-b border-[#cad5e2]">
+        <Image
+          src="/user-empty-banner.webp"
+          alt="user banner"
+          width={1500}
+          height={160}
+          className="h-full w-full object-cover"
+        />
+      </div>
       <div className="mr-15 ml-9 flex gap-6">
         <SidebarSection />
         {children}

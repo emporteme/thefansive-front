af48e217ea5c228a7021065b72dbe81f9a35041e
Tamerlan Telgarin <t.telgarin@qomek.kz>
2025-08-26 09:34:39 +0500

    feature/KAN-23: route to profile on login click

--- src/containers/layout/header/components/sections/bottom-section/ui/auth.tsx
+++ src/containers/layout/header/components/sections/bottom-section/ui/auth.tsx
@@ -1,9 +1,21 @@
+import { useRouter } from "next/navigation"
 import React from "react"
+import { useRoutes } from "@/shared/hooks/client/use-routes"
 import { Auth as AuthIcon } from "@/shared/icons"
 
 const Auth: React.FC = () => {
+  const routes = useRoutes()
+  const router = useRouter()
+
+  const handleAuth = () => {
+    router.push(routes.user.profile())
+  }
+
   return (
-    <div className="flex cursor-pointer items-center gap-2.5 rounded-lg px-3 py-3 text-gray-700 transition-all duration-200 hover:bg-gray-100 lg:px-4">
+    <div
+      className="flex cursor-pointer items-center gap-2.5 rounded-lg px-3 py-3 text-gray-700 transition-all duration-200 hover:bg-gray-100 lg:px-4"
+      onClick={handleAuth}
+    >
       <p className="hidden font-semibold lg:block">Login</p>
       <AuthIcon className="h-5 w-5" />
     </div>

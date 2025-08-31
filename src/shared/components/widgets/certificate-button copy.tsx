a00374edbc7480a61a250939a2b4f3a9950a2c36
Tamerlan Telgarin <t.telgarin@qomek.kz>
2025-08-31 05:37:21 +0500

    feature/KAN-26: fan support page

---
+++ src/shared/components/widgets/certificate-button copy.tsx
@@ -0,0 +1,38 @@
+import { Certificate as CertificateIcon } from "@/shared/icons"
+import { cn } from "@/shared/lib/utils"
+
+type CertificateType = "fan" | "donation"
+
+interface CertificateData {
+  label: string
+}
+
+interface CertificateButtonProps {
+  type: CertificateType
+  className?: string
+  onClick?: () => void
+}
+
+const dataByType = {
+  fan: {
+    label: "Fan Certificate",
+  },
+} as Record<CertificateType, CertificateData>
+
+const CertificateButton: React.FC<CertificateButtonProps> = ({ type, className, onClick, ...props }) => {
+  return (
+    <div
+      className={cn(
+        "flex w-full flex-1 cursor-pointer items-center justify-center gap-1 rounded-xl bg-slate-200 p-2 text-sm font-normal text-black",
+        className
+      )}
+      onClick={onClick}
+      {...props}
+    >
+      <CertificateIcon />
+      <span>{dataByType?.[type]?.label}</span>
+    </div>
+  )
+}
+
+export { CertificateButton }

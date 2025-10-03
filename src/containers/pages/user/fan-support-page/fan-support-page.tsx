b3f1b95623fd5bf1a59cca7b5ea7ad3b64935392
Tamerlan Telgarin <tkreac@gmail.com>
2025-10-04 00:25:50 +0500

    refactor fan support

--- src/containers/pages/user/fan-support-page/fan-support-page.tsx
+++ src/containers/pages/user/fan-support-page/fan-support-page.tsx
@@ -2,9 +2,109 @@
 
 import { useState } from "react"
 import { Tabs } from "@/shared/components/ui/tabs"
-import { AllTabSection } from "./components/sections/all-tab-section"
-import { CompletedTabSection } from "./components/sections/completed-tab-section"
-import { OngoingTabSection } from "./components/sections/ongoing-tab-section"
+import { DeliveryStatus, PaymentStatus } from "@/shared/types/order"
+import { TabSection } from "./components/sections/tab-section"
+
+export interface FanSupportOrder {
+  id: string
+  date: string
+  collection: string
+  title: string
+  paymentStatus: PaymentStatus
+  deliveryStatus: DeliveryStatus
+  estimatedDelivery: string
+  carrier: string
+  carrierImage: string
+  code: string
+  image: string
+}
+
+const orders = {
+  all: [
+    {
+      id: "1234567899",
+      date: "10/08/2025",
+      collection: "Liverpool Fan Support",
+      title: "LFC Signed 25/26 Wirtz Boxed Shirt",
+      paymentStatus: "paid" as PaymentStatus,
+      deliveryStatus: "delivered" as DeliveryStatus,
+      estimatedDelivery: "24/06 - 26/06",
+      carrier: "DHL",
+      carrierImage: "/images/dev/dhl.png",
+      code: "1234567890",
+      image: "/images/dev/liverpool-fan-support.png",
+    },
+    {
+      id: "1234567890",
+      date: "10/08/2025",
+      collection: "Liverpool Fan Support",
+      title: "LFC Signed Gerrard Ball In Case",
+      paymentStatus: "paid" as PaymentStatus,
+      deliveryStatus: "preparing" as DeliveryStatus,
+      estimatedDelivery: "24/06 - 26/06",
+      carrier: "DHL",
+      carrierImage: "/images/dev/dhl.png",
+      code: "1234567890",
+      image: "/images/dev/liverpool-fan-ball.png",
+    },
+    {
+      id: "1234567891",
+      date: "10/08/2025",
+      collection: "AZ Cardinals Fan Support",
+      title: "Arizona Cardinals Fan Ring",
+      paymentStatus: "paid" as PaymentStatus,
+      deliveryStatus: "shipped" as DeliveryStatus,
+      estimatedDelivery: "24/06 - 26/06",
+      carrier: "DHL",
+      carrierImage: "/images/dev/dhl.png",
+      code: "1234567891",
+      image: "/images/dev/ring-fan.png",
+    },
+  ],
+  ongoing: [
+    {
+      id: "1234567890",
+      date: "10/08/2025",
+      collection: "Liverpool Fan Support",
+      title: "LFC Signed Gerrard Ball In Case",
+      paymentStatus: "paid" as PaymentStatus,
+      deliveryStatus: "preparing" as DeliveryStatus,
+      estimatedDelivery: "24/06 - 26/06",
+      carrier: "DHL",
+      carrierImage: "/images/dev/dhl.png",
+      code: "1234567890",
+      image: "/images/dev/liverpool-fan-ball.png",
+    },
+    {
+      id: "1234567891",
+      date: "10/08/2025",
+      collection: "AZ Cardinals Fan Support",
+      title: "Arizona Cardinals Fan Ring",
+      paymentStatus: "paid" as PaymentStatus,
+      deliveryStatus: "shipped" as DeliveryStatus,
+      estimatedDelivery: "24/06 - 26/06",
+      carrier: "DHL",
+      carrierImage: "/images/dev/dhl.png",
+      code: "1234567891",
+      image: "/images/dev/ring-fan.png",
+    },
+  ],
+  completed: [
+    {
+      id: "1234567899",
+      date: "10/08/2025",
+      collection: "Liverpool Fan Support",
+      title: "LFC Signed 25/26 Wirtz Boxed Shirt",
+      paymentStatus: "paid" as PaymentStatus,
+      deliveryStatus: "delivered" as DeliveryStatus,
+      estimatedDelivery: "24/06 - 26/06",
+      carrier: "DHL",
+      carrierImage: "/images/dev/dhl.png",
+      code: "1234567890",
+      image: "/images/dev/liverpool-fan-support.png",
+    },
+  ],
+} as const satisfies Record<string, FanSupportOrder[]>
 
 const FanSupportPage: React.FC = () => {
   const [activeTab, setActiveTab] = useState<string>("all")
@@ -17,9 +117,9 @@ const FanSupportPage: React.FC = () => {
     <div className="flex w-full flex-col">
       <Tabs
         tabs={[
-          { label: "All", value: "all", content: <AllTabSection /> },
-          { label: "Ongoing", value: "ongoing", content: <OngoingTabSection /> },
-          { label: "Completed", value: "completed", content: <CompletedTabSection /> },
+          { label: "All", value: "all", content: <TabSection orders={orders.all} /> },
+          { label: "Ongoing", value: "ongoing", content: <TabSection orders={orders.ongoing} /> },
+          { label: "Completed", value: "completed", content: <TabSection orders={orders.completed} /> },
         ]}
         activeTab={activeTab}
         onTabChange={handleTabChange}

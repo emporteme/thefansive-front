41fe5efe0ff1c99fca2e9e98e6ad043f4469058a
Tamerlan Telgarin <tkreac@gmail.com>
2025-10-09 16:42:30 +0500

    or

--- src/shared/components/widgets/auth-modal/ui/or-divider.tsx
+++ src/shared/components/widgets/auth-modal/ui/or-divider.tsx
@@ -1,13 +1,17 @@
 import React from "react"
 
-export const OrDivider = () => {
+interface OrDividerProps {
+  text: string
+}
+
+export const OrDivider = ({ text }: OrDividerProps) => {
   return (
     <div className="relative">
       <div className="absolute inset-0 flex items-center">
         <div className="w-full border-t border-slate-300"></div>
       </div>
       <div className="relative flex justify-center text-sm">
-        <span className="bg-white px-2 text-sm text-slate-500">or continue with Email</span>
+        <span className="bg-white px-2 text-sm text-slate-500">{text}</span>
       </div>
     </div>
   )

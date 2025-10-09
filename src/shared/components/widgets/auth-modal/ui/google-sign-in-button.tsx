eac351980cc489888aae0d4cf510bf2dcf3da130
Tamerlan Telgarin <tkreac@gmail.com>
2025-10-09 16:46:09 +0500

    GoogleSignInButton

--- src/shared/components/widgets/auth-modal/ui/google-sign-in-button.tsx
+++ src/shared/components/widgets/auth-modal/ui/google-sign-in-button.tsx
@@ -1,11 +1,11 @@
+import Image from "next/image"
 import React from "react"
-import { Google } from "@/shared/icons"
 
 export const GoogleSignInButton = () => {
   return (
     <button className="mx-auto flex w-fit cursor-pointer items-center justify-center gap-3 rounded-xl bg-slate-100 px-4 py-4 text-slate-700 transition-all duration-200 hover:bg-slate-200">
-      <Google className="h-6 w-6" />
-      <span className="text-base font-medium">Sign in with Google</span>
+      <Image src="/images/icons/google.svg" alt="Google" width={24} height={24} />
+      <span className="text-lg font-semibold">Sign in with Google</span>
     </button>
   )
 }

132288b1eb96c23a4cddc37e38fb26bac2a48e28
Tamerlan Telgarin <tkreac@gmail.com>
2025-10-10 12:08:52 +0500

    fix GoogleSignInButton

--- src/shared/components/widgets/auth-modal/ui/google-sign-in-button.tsx
+++ src/shared/components/widgets/auth-modal/ui/google-sign-in-button.tsx
@@ -3,7 +3,10 @@ import React from "react"
 
 export const GoogleSignInButton = () => {
   return (
-    <button className="mx-auto flex w-fit cursor-pointer items-center justify-center gap-3 rounded-xl bg-slate-100 px-4 py-4 text-slate-700 transition-all duration-200 active:bg-slate-50">
+    <button
+      type="button"
+      className="mx-auto flex w-fit cursor-pointer items-center justify-center gap-3 rounded-xl bg-slate-100 px-4 py-4 text-slate-700 transition-all duration-200 active:bg-slate-50"
+    >
       <Image src="/images/icons/google.svg" alt="Google" width={24} height={24} />
       <span className="text-lg font-semibold">Sign in with Google</span>
     </button>

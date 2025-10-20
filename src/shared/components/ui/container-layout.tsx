7a4550b5fd63fe28a1228cd76e8fb54e63531893
Tamerlan Telgarin <tkreac@gmail.com>
2025-10-20 14:52:18 +0500

    comment

--- src/shared/components/ui/container-layout.tsx
+++ src/shared/components/ui/container-layout.tsx
@@ -4,6 +4,11 @@ interface ContainerLayoutProps {
   children: React.ReactNode
   className?: string
 }
+
+/*
+  Desktop content max width: 1240px; +64px paddings; max: 1304px;
+*/
+
 export default function ContainerLayout({ children, className }: ContainerLayoutProps) {
-  return <div className={cn("mx-auto w-full max-w-[1440px] px-4 md:px-8 2xl:px-25", className)}>{children}</div>
+  return <div className={cn("mx-auto box-border w-full max-w-[1304px] px-4 sm:px-8", className)}>{children}</div>
 }

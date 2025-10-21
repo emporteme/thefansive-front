d5ab2b381077955ab6e0eba8541bc0e4e35d1ab8
Tamerlan Telgarin <tkreac@gmail.com>
2025-10-21 21:10:15 +0500

    fix cursor

--- src/shared/components/widgets/choose-your-team-modal/favorite-team-card.tsx
+++ src/shared/components/widgets/choose-your-team-modal/favorite-team-card.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import { useCurrentLocale } from "@/locale/client"
+import { cn } from "@/shared/lib"
 import { EmptyTeam, FavoriteTeam } from "@/shared/types/team"
 
 interface FavoriteTeamCardProps {
@@ -25,7 +26,12 @@ const FavoriteTeamCard: React.FC<FavoriteTeamCardProps> = ({ team, onClick, clas
 
   return (
     <div
-      className={`flex h-20 w-20 flex-shrink-0 cursor-pointer scroll-mx-2.5 items-center justify-center rounded-full border border-[#CAD5E2] bg-white ${className}`}
+      className={cn(
+        `flex h-20 w-20 flex-shrink-0 cursor-pointer scroll-mx-2.5 items-center justify-center rounded-full border border-[#CAD5E2] bg-white ${className}`,
+        {
+          "cursor-default": !("team" in team),
+        }
+      )}
       onClick={handleClick}
     >
       <div className={`h-13.5 w-11.5 overflow-hidden rounded-lg`}>

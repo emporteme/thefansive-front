13491a22b62b0a78344a3101405399c944ab1de0
Tamerlan Telgarin <tkreac@gmail.com>
2025-10-21 21:12:28 +0500

    skeleton

--- src/containers/pages/user/profile-page/components/ui/index.ts
+++ src/containers/pages/user/profile-page/components/ui/index.ts
@@ -1,2 +1,3 @@
 export { AddFavoriteClubs } from "./add-favorite-clubs"
 export { FavoriteTeamCard } from "./favorite-team-card"
+export { FavoriteTeamCardSkeleton } from "./favorite-team-card-skeleton"

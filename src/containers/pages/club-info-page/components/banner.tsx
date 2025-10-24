51c34b332b9cba99feb3b29e6c8341ab7a4c5c40
Tamerlan Telgarin <tkreac@gmail.com>
2025-10-25 01:17:08 +0500

    banner

--- src/containers/pages/club-info-page/components/banner.tsx
+++ src/containers/pages/club-info-page/components/banner.tsx
@@ -4,19 +4,19 @@ import ContainerLayout from "@/shared/components/ui/container-layout"
 
 const Banner = () => {
   return (
-    <ContainerLayout className="py-4">
-      <div className="mb-3">
-        <h2 className="mb-3 text-4xl font-semibold">Liverpool Fan Support</h2>
-        <p>
+    <ContainerLayout>
+      <div className="mb-6">
+        <h2 className="mb-3 text-4xl leading-[1.4] font-bold tracking-normal text-slate-900">Liverpool Fan Support</h2>
+        <p className="text-xl leading-[1.4] font-medium tracking-normal text-slate-900">
           Support the club you love and, by going beyond just being a spectator, actively contribute to its success both
           on and off the field.
         </p>
       </div>
       <Image
-        src="/images/dev/banner-team.jpg"
+        src="/images/dev/club-fan-support-banner.png"
         alt="Banner Team"
         width={1000}
-        height={1000}
+        height={375}
         className="block h-auto w-full rounded-[20px]"
       />
     </ContainerLayout>

75e1fb7ac2b68fcce91931c27720c93a4719da05
Tamerlan Telgarin <tkreac@gmail.com>
2025-10-24 22:05:58 +0500

    clubs

--- src/containers/pages/club-info-page/club-info-page.tsx
+++ src/containers/pages/club-info-page/club-info-page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useParams } from "next/navigation"
-import { useTeam } from "@/shared/api/hooks"
+import { useProducts, useTeam } from "@/shared/api/hooks"
 import Banner from "./components/banner"
 import Donate from "./components/donate"
 import Hero from "./components/hero"
@@ -9,6 +9,9 @@ import Product from "./components/product"
 const ClubInfoPage = () => {
   const params = useParams<{ id: string }>()
   const { data: team } = useTeam(Number(params?.id))
+  const { data: products } = useProducts({ teamId: Number(params?.id) })
+
+  console.log(products)
 
   return (
     <>
